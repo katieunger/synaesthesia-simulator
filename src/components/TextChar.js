919a6912c7886516f3b6db55749554e3f0878903
Katie Unger <katie.e.unger@gmail.com>
2019-04-09 23:57:25 -0400

    trying different approaching to rendering letters without losing markup; trying CSS variables; regexp

--- src/components/TextChar.js
+++ src/components/TextChar.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
-const TextChar = ({char, color}) => {
-    return <span style={{color: `${color}`}}>{char}</span>;
+const TextChar = (props) => {
+    console.log(props.children);
+    console.log(props.alphabet);
+
+    const renderedText = props.children.map((child) => {
+      let pattern = new RegExp('[a-zA-Z]', 'g');
+      console.log(pattern);
+      let index = child.props.children.search(pattern);
+      console.log(index);
+      // const replacedText = child.map((text) => {
+
+      // })
+      return child.props.children.replace(pattern, `${props.alphabet[index].letter}`);
+    })
+    // const aPatt = new RegExp('a', 'g');
+    // const renderedText = props.children.map((child)=> {
+    //    return child.props.children.replace(aPatt, )
+    // })
+    return (
+        <div className="ui segment">
+          {renderedText}
+        </div>
+      );
+
+      
+    //return <span style={{color: `${color}`}}>{char}</span>;
 }
 
 export default TextChar;
--- src/components/TextDisplay.js
+++ src/components/TextDisplay.js
@@ -2,22 +2,25 @@ import React from 'react';
 import TextChar from './TextChar';
 
 const TextDisplay = ({alphabet}) => {
-    let text = "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin."
-    let textArr = text.split("");
-    const renderedText = textArr.map((char) => {
-        const colorArr = alphabet.map((item) => {
-            if (item.letter === char.toLowerCase()) {
-                return <TextChar char={char} color={item.color}/>
-            }
-            return null;
-        });
-        return colorArr;
-    })
+    // let text = "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin."
+    // let textArr = text.split("");
+    // const renderedText = textArr.map((char) => {
+    //     const colorArr = alphabet.map((item) => {
+    //         if (item.letter === char.toLowerCase()) {
+    //             return <TextChar char={char} color={item.color}/>
+    //         }
+    //         return null;
+    //     });
+    //     return colorArr;
+    // })
 
     return (
-        <div className="ui segment" id="textDisplay" style={{fontSize: "30px"}}>
-            {renderedText}
-        </div>
+        <TextChar alphabet={alphabet}>
+            <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p>
+            <p>He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.</p>
+            <p>His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me? " he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.</p>
+            <p>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer.</p>
+        </TextChar>
     );
 }
 
